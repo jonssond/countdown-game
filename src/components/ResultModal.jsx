076812e7ae4bfc54fc103080919dd615edd8c350
Diogo Jonsson <diogo.jonsson@hotmail.com>
2024-05-06 18:52:46 -0300

    added portals

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,4 +1,5 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
+import { createPortal } from "react-dom";
 
 const ResultModal = forwardRef(function ResultModal(
   { targetTime, remainingTime, onReset },
@@ -18,7 +19,7 @@ const ResultModal = forwardRef(function ResultModal(
     };
   });
 
-  return (
+  return createPortal(
     <dialog ref={dialog} className="result-modal" onClose={onReset}>
       {userLost ? <h2>You lost</h2> : <h2>Your score: {score}%</h2>}
       <p>
@@ -31,7 +32,8 @@ const ResultModal = forwardRef(function ResultModal(
       <form action="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
-    </dialog>
+    </dialog>,
+    document.getElementById("modal")
   );
 });
 
